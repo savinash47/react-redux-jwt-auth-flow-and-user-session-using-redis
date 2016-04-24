c88e165bb0a02175383a4c96f8e263e41d364039
Avinash Sharma <savinash47@gmail.com>
2016-04-23 17:01:23 -0700

    Added protected route on the main page

--- components/App.js
+++ components/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router'
+import RaisedButton from 'material-ui/lib/raised-button';
 
 const App = React.createClass({
 	render() {
 		return (
 			<div>
+				{this.props.children || <span>
+				<h2>This is home page</h2>
 				<p>Click on Login to go to login page</p>
-				<Link to='/login'>Login</Link>
-				<p>Styling is yet to be done</p>
-				<p>There is still a functionality of protecting the personal routes is left.
-				 This can be done by checking if user is authenticated by making a server request whenever a 
-				 personal component mounts</p>
-
-				<p>Missing parts will be out soon</p>
+				<Link to="/login"><RaisedButton primary={true} label="Login" /></Link> 
+				<br />
+				<br />
+				<Link to="/main"><RaisedButton primary={true} label="Main Page" /></Link>
+				<p>When you click on the main page without login it will take you to login page</p>
+				<p>But when you you are logged in it takes you to the main page</p>
+				</span>}
 			</div>
 		);
 	}
