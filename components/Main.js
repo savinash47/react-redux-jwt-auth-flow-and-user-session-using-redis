45a9a9e30a6e4f2d4222f4cfabe06f9cd9d2b3ba
Avinash Sharma <savinash47@gmail.com>
2016-04-23 17:02:27 -0700

    Added router for home page used material ui on all the pages

--- components/Main.js
+++ components/Main.js
@@ -2,6 +2,10 @@ import React from 'react'
 import logout from '../redux/actions/logout.js'
 import refresh from '../redux/actions/refresh.js'
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import RaisedButton from 'material-ui/lib/raised-button';
+import { Link } from 'react-router'
+
 
 const Main = React.createClass({
 	handleSubmit(event) {
@@ -10,10 +14,15 @@ const Main = React.createClass({
 	},
 
 	componentDidMount() {
+		console.log('token' + localStorage.getItem('token'))
+		if(localStorage.getItem('token') === null){
+			browserHistory.push('/login')
+		} 
 		this.refresher = setInterval(this.refreshSession,20000)
 	},
 
 	refreshSession() {
+		console.log('called')
 		this.props.dispatch(refresh())
 	},
 
@@ -28,7 +37,9 @@ const Main = React.createClass({
 				<p>This page will make a request to the server every 20 seconds to refresh the token for the user so the user
 				can stay signed in as long as user visits every 5 minutes to the webiste. Usually it should be 7 days and token
 				should be refreshed every hour. But for demo purposes it is used as 5 minutes and 20 seconds refresh interval</p>
-				<button type="Submit" onClick={this.handleSubmit}>Logout</button>
+				<RaisedButton label="Logout" primary={true} type="Submit" onClick={this.handleSubmit} />
+
+				<Link to="/"><RaisedButton label="Home Page" secondary={true} /></Link>
 			</div>
 		);
 	}
