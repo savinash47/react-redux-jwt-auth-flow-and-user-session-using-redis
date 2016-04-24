0bd418d1d3e5a9af8d8473aa9a2fefbb37575c76
Avinash Sharma <savinash47@gmail.com>
2016-04-23 17:01:47 -0700

    added validation on component mount

--- components/LogIn.js
+++ components/LogIn.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import login from '../redux/actions/login.js'
+import RaisedButton from 'material-ui/lib/raised-button';
+import TextField from 'material-ui/lib/text-field';
+import { browserHistory } from 'react-router'
 
 const LogIn = React.createClass({
+
+	getInitialState() {
+		return {
+			email: '',
+			password: ''
+		}
+	},
+
+	componentDidMount() {
+		//do more like calling a endpoint and validating token
+		if(localStorage.getItem('token') !== null){
+			browserHistory.push('/main')
+		} 
+	},
+	handleEmail(event) {
+		this.setState({
+			email: event.target.value
+		})
+	},
+
+	handlePassword(event) {
+		this.setState({
+			password:  event.target.value
+		})
+	},
+
 	handleSubmit(event) {
 		event.preventDefault()
 		console.log('submitted')
-		let email = this.refs.email.value;
-		let password = this.refs.password.value;
+		let email = this.state.email;
+		let password = this.state.password;
 		let userData = {
 			email: email,
 			password: password
@@ -20,11 +49,11 @@ const LogIn = React.createClass({
 			<div>
 				<h1>Login</h1>
 				<form onSubmit={this.handleSubmit}>
-					<input type="text" placeholder="email" ref="email" required></input>
-					<input type="password" placeholder="password" ref="password" required></input>
+					<TextField type="text" hintText="email" value={this.state.email} onChange={this.handleEmail} required /> <br />
+					<TextField type="password" hintText="password" vale={this.state.password} onChange={this.handlePassword} required />
 					<p>email is <strong>email@email.com</strong></p>
 					<p>password is <strong>password1@</strong></p>
-					<button type="submit">login</button>
+					<RaisedButton label="Login" secondary={true} type="submit" />
 				</form>
 			</div>
 		);
